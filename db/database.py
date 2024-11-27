4228fb5a490111c4fbda5270fd85067603560c87
Dmytro <dsumcow@gmail.com>
2024-11-27 14:08:23 +0200

    add initialization of tables

--- db/database.py
+++ db/database.py
@@ -1,6 +1,8 @@
 ﻿from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
+
 from config import DATABASE_URL
+from models.news import News
 
 class Database:
     _instance = None
@@ -14,5 +16,10 @@ class Database:
 
     def get_session(self) -> AsyncSession:
         return self._session()
+
+    async def init_tables(self):
+        """Initialization of tables in the database"""
+        async with self._engine.begin() as conn:
+            await conn.run_sync(News.metadata.create_all)  # Create tables
 
 database = Database()
