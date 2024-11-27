55f806cc60ba6934573b5c85a76cae3472d296f1
Dmytro <dsumcow@gmail.com>
2024-11-27 13:14:12 +0200

    add new unit tests

--- tests/test_news.py
+++ tests/test_news.py
@@ -1,21 +1,60 @@
-﻿from fastapi.testclient import TestClient
+﻿import pytest
+from uuid import uuid4
+from unittest.mock import AsyncMock
 
-import db.database
-from main import app
+from db.database import Database
+from schemas.news_schema import NewsCreate, NewsUpdate
+from services.news_service import NewsService
 
-client = TestClient(app)
+@pytest.mark.asyncio
+async def test_list_news():
+    service = NewsService()
+    service.repository.get_list_news = AsyncMock(return_value=[])
+    result = await service.list_news()
+    assert result == []
+    service.repository.get_list_news.assert_called_once()
 
-# def test_create_news(): # Temporary commented because of outdated code
-#     response = client.post("/api/news/", json={
-#         "title": "Test News",
-#         "author": "Author Name",
-#         "description": "A description",
-#         "published_year": 2023
-#     })
-#     assert response.status_code == 200
-#     assert response.json()["title"] == "Test News"
+@pytest.mark.asyncio
+async def test_add_news():
+    service = NewsService()
+    news_data = NewsCreate(
+        title="Test Title",
+        description="Test Description",
+        content="Test Content",
+        publishedAt="2024-11-26",
+        source={"name": "Test Source"},
+    )
+    mock_news = news_data.dict()
+    mock_news["id"] = uuid4()
 
-def test_is_database_singlton():
-    db1 = db.database.database.__hash__()
-    db2 = db.database.database.__hash__()
-    assert db1 == db2
+    service.repository.create_news = AsyncMock(return_value=mock_news)
+    result = await service.add_news(news_data)
+    assert result == mock_news
+    service.repository.create_news.assert_called_once_with(news_data)
+
+@pytest.mark.asyncio
+async def test_is_database_signleton():
+    db = Database()
+    db1 = Database()
+    assert db.__hash__() == db1.__hash__()
+
+@pytest.mark.asyncio
+async def test_get_news():
+    service = NewsService()
+    news_id = uuid4()
+    mock_news = {"id": news_id, "title": "Test Title"}
+
+    service.repository.get_news = AsyncMock(return_value=mock_news)
+    result = await service.get_news(news_id)
+    assert result == mock_news
+    service.repository.get_news.assert_called_once_with(news_id)
+
+@pytest.mark.asyncio
+async def test_update_news():
+    service = NewsService()
+    news_id = uuid4()
+    update_data = NewsUpdate(title="Updated Title")
+    service.repository.update_news = AsyncMock(return_value=update_data)
+    result = await service.update_news(news_id, update_data)
+    assert result == update_data
+    service.repository.update_news.assert_called_once_with(news_id, update_data)
